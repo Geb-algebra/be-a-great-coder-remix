92c4acb1e02ef0f99b8ef22ef01b509eaf47ce34
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-01-08 14:46:29 +0900

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+    <main className="">
+      Hello
+    </main>
+  );
+}

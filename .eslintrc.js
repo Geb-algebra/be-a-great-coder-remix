5b4afb800473caf475c008953d4696c1c43c0b61
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-02-23 09:05:38 +0900

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,12 @@ module.exports = {
     '@remix-run/eslint-config',
     '@remix-run/eslint-config/node',
     // '@remix-run/eslint-config/jest-testing-library',
-    'testing-library',
     'prettier',
   ],
   env: {
     'cypress/globals': true,
   },
-  plugins: ['cypress'],
+  plugins: ['cypress', 'testing-library'],
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it means we have to explicitly
   // set the jest version.

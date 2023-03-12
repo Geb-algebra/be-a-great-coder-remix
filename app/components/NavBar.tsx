f33ee065ae7167e2da58318a354a8db74daa5bd9
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-03-12 11:04:03 +0900

    typecheck / remove unused codes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function NavBar(props: {}) {
+export default function NavBar(props: { children: React.ReactNode }) {
   return (
     <div className="flex w-full bg-geb-blue">
       <h1 className="mx-6 h-12 align-middle text-4xl font-semibold">Be A Great Coder</h1>

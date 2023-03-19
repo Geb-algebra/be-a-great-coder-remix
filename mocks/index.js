2001f0a9d22bb359ae2696b841f05b020875429b
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2023-03-19 17:47:28 +0900

    add detailed-problems mock

--- mocks/index.js
+++ mocks/index.js
@@ -1,9 +1,9 @@
 const { rest } = require('msw');
 const { setupServer } = require('msw/node');
 const { atcoderUserPageMock } = require('./atcoder-user-page');
-const { USERNAME, PASSWORD } = require('./consts');
+const { detailedProblemsMock } = require('./detailed-problems');
 
-const server = setupServer(atcoderUserPageMock);
+const server = setupServer(atcoderUserPageMock, detailedProblemsMock);
 
 server.listen({ onUnhandledRequest: 'warn' });
 console.info('🔶 Mock server running');
